0001ff8fc5f47558a0669775f75dbac86ba923d5
dongbao <zld126126@126.com>
2021-08-02 15:46:53 +0800

    implement a-star

---
+++ MyAStar/Assets/Scripts/FindPath.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FindPath : MonoBehaviour {
+	private Grid grid;
+
+	// Use this for initialization
+	void Start () {
+		grid = GetComponent<Grid> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FindingPath (grid.player.position, grid.destPos.position);
+	}
+
+	// A*寻路
+	void FindingPath(Vector3 s, Vector3 e) {
+		Grid.NodeItem startNode = grid.getItem (s);
+		Grid.NodeItem endNode = grid.getItem (e);
+
+		List<Grid.NodeItem> openSet = new List<Grid.NodeItem> ();
+		HashSet<Grid.NodeItem> closeSet = new HashSet<Grid.NodeItem> ();
+		openSet.Add (startNode);
+
+		while (openSet.Count > 0) {
+			Grid.NodeItem curNode = openSet [0];
+
+			for (int i = 0, max = openSet.Count; i < max; i++) {
+				if (openSet [i].fCost <= curNode.fCost &&
+				    openSet [i].hCost < curNode.hCost) {
+					curNode = openSet [i];
+				}
+			}
+
+			openSet.Remove (curNode);
+			closeSet.Add (curNode);
+
+			// 找到的目标节点
+			if (curNode == endNode) {
+				generatePath (startNode, endNode);
+				return;
+			}
+
+			// 判断周围节点，选择一个最优的节点
+			foreach (var item in grid.getNeibourhood(curNode)) {
+				// 如果是墙或者已经在关闭列表中
+				if (item.isWall || closeSet.Contains (item))
+					continue;
+				// 计算当前相领节点现开始节点距离
+				int newCost = curNode.gCost + getDistanceNodes (curNode, item);
+				// 如果距离更小，或者原来不在开始列表中
+				if (newCost < item.gCost || !openSet.Contains (item)) {
+					// 更新与开始节点的距离
+					item.gCost = newCost;
+					// 更新与终点的距离
+					item.hCost = getDistanceNodes (item, endNode);
+					// 更新父节点为当前选定的节点
+					item.parent = curNode;
+					// 如果节点是新加入的，将它加入打开列表中
+					if (!openSet.Contains (item)) {
+						openSet.Add (item);
+					}
+				}
+			}
+		}
+
+		generatePath (startNode, null);
+	}
+
+	// 生成路径
+	void generatePath(Grid.NodeItem startNode, Grid.NodeItem endNode) {
+		List<Grid.NodeItem> path = new List<Grid.NodeItem>();
+		if (endNode != null) {
+			Grid.NodeItem temp = endNode;
+			while (temp != startNode) {
+				path.Add (temp);
+				temp = temp.parent;
+			}
+			// 反转路径
+			path.Reverse ();
+		}
+		// 更新路径
+		grid.updatePath(path);
+	}
+
+	// 获取两个节点之间的距离
+	int getDistanceNodes(Grid.NodeItem a, Grid.NodeItem b) {
+		int cntX = Mathf.Abs (a.x - b.x);
+		int cntY = Mathf.Abs (a.y - b.y);
+		// 判断到底是那个轴相差的距离更远
+		if (cntX > cntY) {
+			return 14 * cntY + 10 * (cntX - cntY);
+		} else {
+			return 14 * cntX + 10 * (cntY - cntX);
+		}
+	}
+
+
+}
---
+++ MyAStar/Assets/Scripts/Grid.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+	public GameObject NodeWall;
+	public GameObject Node;
+
+	// 节点半径
+	public float NodeRadius = 0.5f;
+	// 过滤墙体所在的层
+	public LayerMask WhatLayer;
+
+	// 玩家
+	public Transform player;
+	// 目标
+	public Transform destPos;
+
+
+	/// <summary>
+	/// 寻路节点
+	/// </summary>
+	public class NodeItem {
+		// 是否是墙
+		public bool isWall;
+		// 位置
+		public Vector3 pos;
+		// 格子坐标
+		public int x, y;
+
+		// 与起点的长度
+		public int gCost;
+		// 与目标点的长度
+		public int hCost;
+
+		// 总的路径长度
+		public int fCost {
+			get {return gCost + hCost; }
+		}
+
+		// 父节点
+		public NodeItem parent;
+
+		public NodeItem(bool isWall, Vector3 pos, int x, int y) {
+			this.isWall = isWall;
+			this.pos = pos;
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	private NodeItem[,] grid;
+	private int w, h;
+
+	private GameObject WallRange, PathRange;
+	private List<GameObject> pathObj = new List<GameObject> ();
+
+	void Awake() {
+		// 初始化格子
+		w = Mathf.RoundToInt(transform.localScale.x * 2);
+		h = Mathf.RoundToInt(transform.localScale.y * 2);
+		grid = new NodeItem[w, h];
+
+		WallRange = new GameObject ("WallRange");
+		PathRange = new GameObject ("PathRange");
+
+		// 将墙的信息写入格子中
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				Vector3 pos = new Vector3 (x*0.5f, y*0.5f, -0.25f);
+				// 通过节点中心发射圆形射线，检测当前位置是否可以行走
+				bool isWall = Physics.CheckSphere (pos, NodeRadius, WhatLayer);
+				// 构建一个节点
+				grid[x, y] = new NodeItem (isWall, pos, x, y);
+				// 如果是墙体，则画出不可行走的区域
+				if (isWall) {
+					GameObject obj = Instantiate (NodeWall, pos, Quaternion.identity) as GameObject;
+					obj.transform.SetParent (WallRange.transform);
+				}
+			}
+		}
+
+	}
+
+	// 根据坐标获得一个节点
+	public NodeItem getItem(Vector3 position) {
+		int x = Mathf.RoundToInt (position.x) * 2;
+		int y = Mathf.RoundToInt (position.y) * 2;
+		x = Mathf.Clamp (x, 0, w - 1);
+		y = Mathf.Clamp (y, 0, h - 1);
+		return grid [x, y];
+	}
+
+	// 取得周围的节点
+	public List<NodeItem> getNeibourhood(NodeItem node) {
+		List<NodeItem> list = new List<NodeItem> ();
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				// 如果是自己，则跳过
+				if (i == 0 && j == 0)
+					continue;
+				int x = node.x + i;
+				int y = node.y + j;
+				// 判断是否越界，如果没有，加到列表中
+				if (x < w && x >= 0 && y < h && y >= 0)
+					list.Add (grid [x, y]);
+			}
+		}
+		return list;
+	}
+
+	// 更新路径
+	public void updatePath(List<NodeItem> lines) {
+		int curListSize = pathObj.Count;
+		for (int i = 0, max = lines.Count; i < max; i++) {
+			if (i < curListSize) {
+				pathObj [i].transform.position = lines [i].pos;
+				pathObj [i].SetActive (true);
+			} else {
+				GameObject obj = Instantiate(Node, lines[i].pos, Quaternion.identity) as GameObject;
+				obj.transform.SetParent (PathRange.transform);
+				pathObj.Add (obj);
+			}
+		}
+		for (int i = lines.Count; i < curListSize; i++) {
+			pathObj [i].SetActive (false);
+		}
+	}
+}
