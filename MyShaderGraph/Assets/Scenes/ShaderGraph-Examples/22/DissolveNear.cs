e675d485579d7c85eaaa7d1b621ec63657570184
landon <zld126126@126.com>
2021-08-20 13:54:06 +0800

    update project

---
+++ MyShaderGraph/Assets/Scenes/ShaderGraph-Examples/22/DissolveNear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveNear : MonoBehaviour
+{
+    /// <summary>
+    /// 材质球
+    /// </summary>
+    public Material mat;
+    /// <summary>
+    /// 要靠近过来的物体
+    /// </summary>
+    public Transform m_targetObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetVector("Vector3_1A5E88B8", m_targetObj.position);
+    }
+}
