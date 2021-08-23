f32b433004a43a50c2f3afa377677dfa00dc2086
dongbao <zld126126@126.com>
2021-08-23 17:34:33 +0800

    update reverse project

---
+++ MyReverseCode/Assets/Scenes/Sample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    private void Log(string s)
+    {
+        Debug.Log(DateTime.Now.ToString() + ":" + s);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Log("Sample Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
