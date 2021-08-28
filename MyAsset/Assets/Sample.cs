1f3a2ee42eb2a3861a6539c7f32c2d8fc57c5079
dongbao <zld126126@126.com>
2021-08-28 11:31:07 +0800

    update project

---
+++ MyAsset/Assets/Sample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyAsset.MyAsset _asset = Resources.Load<MyAsset.MyAsset>(MyAsset.MyAssetHelper.GetFileName());
+        Debug.Log(_asset.Author.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
