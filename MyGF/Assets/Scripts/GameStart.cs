d94aebbd22e6cee854af5989208083bcaf0d774e
zhangliandong <zhangliandong@gyyx.cn>
2022-02-16 18:32:16 +0800

    update

---
+++ MyGF/Assets/Scripts/GameStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+
+    void Awake()
+    {
+		AssetBundleManager.Instance.LoadAssetBundleConfig();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
