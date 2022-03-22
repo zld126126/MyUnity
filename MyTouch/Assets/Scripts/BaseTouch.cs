c42f3a10b2571376d286447fe70684c352c784f3
landon <zld126126@126.com>
2022-03-22 18:42:15 +0800

    init mytouch

---
+++ MyTouch/Assets/Scripts/BaseTouch.cs
@@ -0,0 +1,45 @@
+﻿/****************************************************
+    文件：BaseTouch.cs
+	功能：基类
+*****************************************************/
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BaseTouch : MonoBehaviour 
+{
+    protected T GetOrAddComponect<T>(GameObject go) where T : Component
+    {
+        T t = go.GetComponent<T>();
+        if (t == null)
+        {
+            t = go.AddComponent<T>();
+        }
+
+        return t;
+    }
+    
+    protected void OnClickDown(GameObject go, Action<PointerEventData> cb)
+    {
+        TouchListener listener = GetOrAddComponect<TouchListener>(go);
+        listener.onClickDown = cb;
+    }
+
+    protected void OnClickUp(GameObject go, Action<PointerEventData> cb)
+    {
+        TouchListener listener = GetOrAddComponect<TouchListener>(go);
+        listener.onClickUp = cb;
+    }
+
+    protected void OnDrag(GameObject go, Action<PointerEventData> cb)
+    {
+        TouchListener listener = GetOrAddComponect<TouchListener>(go);
+        listener.onDrag = cb;
+    }
+    
+    protected void SetActive(Image img, bool state = true) {
+        img.transform.gameObject.SetActive(state);
+    }
+}
+    
