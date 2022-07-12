b65a2a886be8350eedc0f05f6086416efcc96458
landon <zld126126@126.com>
2022-07-12 09:41:20 +0800

    revise:gameframework

---
+++ MyGF/Assets/Scripts/GameEntry.Custom.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+ 
+namespace Tutorial
+{
+    /// <summary>
+    /// 游戏入口。
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour
+    {
+        private static void InitCustomComponents()
+        {
+            // 将来在这里注册自定义的组件
+        }
+ 
+        private static void InitCustomDebuggers()
+        {
+            // 将来在这里注册自定义的调试器
+        }
+    }
+}
