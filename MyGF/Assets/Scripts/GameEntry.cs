32f52e6ec818051a4f63fc273ca8188a792a84c0
landon <zld126126@126.com>
2022-07-12 09:41:20 +0800

    revise:gameframework

---
+++ MyGF/Assets/Scripts/GameEntry.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Tutorial
+{
+    /// <summary>
+    /// 游戏入口。
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour
+    {
+        private void Start()
+        {
+            // 初始化内置组件
+            InitBuiltinComponents();
+
+            // 初始化自定义组件
+            InitCustomComponents();
+
+            // 初始化自定义调试器
+            InitCustomDebuggers();
+        }
+    }
+}
--- MyGF/readme.md
+++
@@ -1,3 +0,0 @@
-# GameFramework思想
-https://zhuanlan.zhihu.com/p/64984309
-https://www.zhihu.com/column/max1993
