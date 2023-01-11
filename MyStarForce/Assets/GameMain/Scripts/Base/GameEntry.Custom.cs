6e1e08905ef1035ef819bc509b44c6e5ff8b8bc6
landon <zld126126@126.com>
2023-01-11 10:15:11 +0800

    init MyStarForce

---
+++ MyStarForce/Assets/GameMain/Scripts/Base/GameEntry.Custom.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using UnityEngine;
+
+namespace StarForce
+{
+    /// <summary>
+    /// 游戏入口。
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour
+    {
+        public static BuiltinDataComponent BuiltinData
+        {
+            get;
+            private set;
+        }
+
+        public static HPBarComponent HPBar
+        {
+            get;
+            private set;
+        }
+
+        private static void InitCustomComponents()
+        {
+            BuiltinData = UnityGameFramework.Runtime.GameEntry.GetComponent<BuiltinDataComponent>();
+            HPBar = UnityGameFramework.Runtime.GameEntry.GetComponent<HPBarComponent>();
+        }
+    }
+}
